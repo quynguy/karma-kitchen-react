336a1dde0686f515dd502f913356e0c6c5fa6789
wquynguy <wquyenle@gmail.com>
2024-01-25 14:39:41 -0600

    added assets images and importing navbar.js rafce

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import About from "./components/About";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import Home from "./components/Home";
+import Navbar from "./components/Navbar";
 import Testimonial from "./components/Testimonial";
 import Work from "./components/Work";
 
