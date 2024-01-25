66c42d6a4aa76f732fc18307c4c4e90abbe66203
wquynguy <wquyenle@gmail.com>
2024-01-25 14:29:37 -0600

    importing components to app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import Testimonial from "./components/Testimonial";
+import Work from "./components/Work";
+
+
 
 function App() {
   return (
     <div className="App">
-      Hello World!
+      <Home />
+      <About />
+      <Work />
+      <Testimonial />
+      <Contact />
+      <Footer />
     </div>
   );
 }
