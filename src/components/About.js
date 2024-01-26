3bbb7f463caf657688737f6051ce16219e716a98
wquynguy <wquyenle@gmail.com>
2024-01-26 10:21:13 -0600

    modified about.js component and added contents to contact.js component

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,33 @@
 import React from 'react'
-
+import AboutBackgroundImage from './../assets/buddha-bowl-1.png';
+import { BsFillPlayCircleFill } from "react-icons/bs";
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div className="about-section-container">
+      <div classname="about-backgroundimage-container">
+        <img src={AboutBackgroundImage} alt="karma-kitchen-buddha-bowl" />
+      </div>
+      <div className="about-section-text-container">
+        <p className="primary-subheading">About</p>
+        <h1 className="primary-heading">
+        Buddha bowls are not only nutritious but also visually appealing, making them an attractive option for both adults and children.
+        </h1>
+        <p className="primary-text">
+        They can be easily customized to suit individual preferences and dietary restrictions, making them a versatile option for various dietary needs.
+        </p>
+        <p className="primary-text">
+        Buddha bowls typically include a variety of vegetables, grains, proteins, and healthy fats, providing a wide range of nutrients essential for overall health.
+        </p>
+        <div className="about-buttons-container">
+          <button className="secondary-button"> Learn More </button>
+          <button className="watch-video-button">
+            <BsFillPlayCircleFill /> Watch Video
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default About
+export default About;
 
