6e02a8139da0724784a26de4c37c6629fd718b75
wquynguy <wquyenle@gmail.com>
2024-01-25 20:00:48 -0600

    added contents to home.js component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import Navbar from './Navbar';
+
+import { FiArrowRight } from "react-icons/fi";
+
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="home-container">
+      <Navbar />
+      <div className="home-text-section">
+        <h1 className="primary-heading">
+        Elevate your party experience with Karma Kitchen's customizable Buddha bowls, crafted with love and served with good karma.  
+        </h1>
+        <p className="primary-text">
+        bursting with flavor and designed to delight every guest. 
+        </p>
+        <button className="secondary-button">
+          Order Now <FiArrowRight /> { " "}
+        </button>
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
